a28c5ec470889705b570ec816405a2f7579477cb
WijayaSaputra-867 <wijayasaputra679@gmail.com>
2023-12-12 16:32:50 +0700

    membuat factory guest dengan faker

--- database/factories/GuestFactory.php
+++ database/factories/GuestFactory.php
@@ -14,10 +14,21 @@ class GuestFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    private function getRandArray($data = []){
+        $count_data = count($data) - 1;
+        $result = $data[mt_rand(0, $count_data)];
+        return $result;
+    }
     public function definition(): array
     {
+        $gender = $this->getRandArray(['Laki-laki', 'Perempuan']);
         return [
-            //
+            'nama_tamu' => fake()->name(),
+            'bertemu' => fake()->name(),
+            'asal_instansi' => fake()->company(),
+            'keperluan_tamu' => fake()->realTextBetween($minNbChars = 10, $maxNbChars = 20, $indexSize = 2),
+            'telepon' => fake()->e164PhoneNumber(),
+            'waktu_datang' => fake()->dateTimeBetween('-5 year', 'now')
         ];
     }
 }
